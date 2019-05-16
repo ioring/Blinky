9db08df8ee048a4e17f3a1ce7ec72ebdd5e6dafa
Iori Suzuki <>
2019-05-16 21:57:30 +0900

    フォルダを更新

---
+++ Blinky/Screens/ChangeNickname/ChangeNickNameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeNickNameViewController.swift
+//  Blinky
+//
+//  Created by Iori Suzuki on 2019/05/16.
+//  Copyright © 2019 Iori Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ChangeNickNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
